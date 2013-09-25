dba399e353cf8a0d522592fdb5f175350d570a8c
nightwishx <nightwishx@gmail.com>
2013-09-25 22:53:02 +0700

    add PostTag model, fix jquery map

---
+++ app/models/PostTag.php
@@ -0,0 +1,16 @@
+<?php
+
+/**
+ * BBCMS - A PHP CMS for web newspapers
+ *
+ * @package  BBCMS
+ * @version  1.0
+ * @author   BinhBEER <binhbeer@taymay.vn>
+ * @link     http://cms.binhbeer.com
+ */
+
+class PostTag extends Eloquent {
+	
+	protected $table = 'post_tag';
+
+}
